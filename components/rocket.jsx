254be96aed565475d191b811c9e408a7aa6f99ad
nanawebdev <nanaprettysinger@gmail.com>
2022-02-06 21:47:24 +0300

    fix imgages src path

--- components/rocket.jsx
+++ components/rocket.jsx
@@ -2,26 +2,26 @@ export default function Rocket() {
     return (
         <div className="rocket">
             <div className="rocket__image rocket__image--1">
-                <img src="/rocket.png" alt="rocket" width={280} height={280} />
+                <img src="./rocket.png" alt="rocket" width={280} height={280} />
             </div>
             <div className="rocket__image rocket__image--2">
-                <img src="/heart.png" alt="heart" width={56} height={56} />
+                <img src="./heart.png" alt="heart" width={56} height={56} />
             </div>
 
             <div className="rocket__image rocket__image--3">
-                <img src="/heart.png" alt="heart" width={56} height={56} />
+                <img src="./heart.png" alt="heart" width={56} height={56} />
             </div>
 
             <div className="rocket__image rocket__image--4">
-                <img src="/heart.png" alt="heart" width={56} height={56} />
+                <img src="./heart.png" alt="heart" width={56} height={56} />
             </div>
 
             <div className="rocket__image rocket__image--5">
-                <img src="/heart.png" alt="heart" width={56} height={56} />
+                <img src="./heart.png" alt="heart" width={56} height={56} />
             </div>
 
             <div className="rocket__image rocket__image--6">
-                <img src="/heart.png" alt="heart" width={56} height={56} />
+                <img src="./heart.png" alt="heart" width={56} height={56} />
             </div>
         </div>
     )
