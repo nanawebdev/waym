1094846e15f6d0bd2ce80b427ade33bee6668aee
nanawebdev <nanaprettysinger@gmail.com>
2022-02-15 16:17:37 +0300

    draft

--- components/layout.jsx
+++ components/layout.jsx
@@ -29,7 +29,7 @@ export default function Layout({ children }) {
                         {children}
                     </main>
                 </div>
-                {/* <Footer /> */}
+                <Footer />
                 {menuIsOpen && <MobileMenu closeMenuFunc={closeMobileMenu} />}
             </div>
         </>
