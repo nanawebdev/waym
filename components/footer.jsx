845e975da230fb8f7542274773e0fc3d57a81183
nanawebdev <nanaprettysinger@gmail.com>
2022-02-16 15:03:21 +0300

    fixies

--- components/footer.jsx
+++ components/footer.jsx
@@ -3,16 +3,37 @@ import GooglePlay from '../public/gp.svg'
 import Facebook from '../public/facebook.svg'
 import Instagram from '../public/instagram.svg'
 import Telegram from '../public/telegram.svg'
+import { useState } from 'react'
 
 export default function Footer() {
+    // const [name, setName] = useState('')
+    // const [question, setQuestion] = useState('')
+    // const [email, setEmail] = useState('')
+
+    // const [nameDirty, setNameDirty] = useState(false)
+    // const [questionDirty, setQuestionDirty] = useState(false)
+    // const [emailDirty, setEmailDirty] = useState(false)
+
+    // const [nameError, setNameError] = useState('Заполните поле')
+    // const [questionError, setQuestionError] = useState('Заполните поле')
+    // const [emailError, setEmailError] = useState('Заполните поле')
+
+// const validateForm = () => {
+
+// }
+
+// const onSubmitForm = (e) => {
+//     e.preventDefault()
+// }
+
     return (
         <footer className="footer section">
             <div className="container">
-                <form>
-                    <input type="text" placeholder="Name" minLength="1" maxLength="100"></input>
-                    <input type="text" placeholder="Question" minLength="1" maxLength="500"></input>
-                    <input type="email" placeholder="Email" minLength="1" maxLength="100"></input>
-                    <button type="submit">Send</button>
+                <form method="POST" action="#">
+                    <input type="text" placeholder="Name" minLength="1" maxLength="100" name="user-name"></input>
+                    <input type="text" placeholder="Question" minLength="1" maxLength="500" name="user-question"></input>
+                    <input type="email" placeholder="Email" minLength="1" maxLength="100" name="user-email"></input>
+                    <button type="submit" onClick={(e) => {}}>Send</button>
                 </form>
                 <section>
                     <h3 className="visually-hidden">Links</h3>
