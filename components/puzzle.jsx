8d8ff6cb84dfda4fc1f9e1c5acd2c13ddf6d505d
nanawebdev <nanaprettysinger@gmail.com>
2022-02-18 20:19:25 +0300

    fix images path

--- components/puzzle.jsx
+++ components/puzzle.jsx
@@ -1,24 +1,36 @@
+import Image from "next/image"
+import Puzzle1 from "./../public/puzzle-1.png"
+import Puzzle2 from "./../public/puzzle-2.png"
+import Puzzle3 from "./../public/puzzle-3.png"
+import Puzzle4 from "./../public/puzzle-4.png"
+import Puzzle5 from "./../public/puzzle-5.png"
+
 export default function Puzzle() {
     return (
         <div className="puzzle">
             <div className="puzzle__item puzzle__item--1">
-                <img src="./puzzle-1.png" alt="puzzle"  />
+                <Image src={Puzzle1} alt="puzzle" />
+                {/* // <img src="./puzzle-1.png" alt="puzzle"  /> */}
             </div>
 
             <div className="puzzle__item puzzle__item--2">
-                <img src="./puzzle-2.png" alt="puzzle" />
+                <Image src={Puzzle2} alt="puzzle" />
+                {/* <img src="./puzzle-2.png" alt="puzzle" /> */}
             </div>
 
             <div className="puzzle__item puzzle__item--3">
-                <img src="./puzzle-3.png" alt="puzzle" />
+                <Image src={Puzzle3} alt="puzzle" />
+                {/* <img src="./puzzle-3.png" alt="puzzle" /> */}
             </div>
 
             <div className="puzzle__item puzzle__item--4">
-                <img src="./puzzle-4.png" alt="puzzle" />
+                <Image src={Puzzle4} alt="puzzle" />
+                {/* <img src="./puzzle-4.png" alt="puzzle" /> */}
             </div>
 
             <div className="puzzle__item puzzle__item--5">
-                <img src="./puzzle-5.png" alt="puzzle" />
+                <Image src={Puzzle5} alt="puzzle" />
+                {/* <img src="./puzzle-5.png" alt="puzzle" /> */}
             </div>
         </div>
     )
